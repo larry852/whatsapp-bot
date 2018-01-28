a4e7e96e37a4f5a2123e2d3d915c66ef5b01634c
larry852 <larryportocarrero@gmail.com>
2018-01-28 15:15:18 -0500

    Funcionalidad completa para envio de mensajes

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from webwhatsapi import WhatsAPIDriver
 
 driver = WhatsAPIDriver(client='chrome')
-
 driver.wait_for_login()
-contacts = driver.get_all_chats()
-contacts[1].send_message("Test")
+contacts = driver.get_contacts()
+print("Contacts: " + str(len(contacts)))
+contacts[11].send_message("Test")
